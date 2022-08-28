6fc28c5fe812b7c5d7fbff6365be758d1f7b5370
Hristiana <2021813@swansea.ac.uk>
2022-08-28 17:13:07 +0100

    Added build and frontend in the path

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+  res.sendFile(`${__dirname}/frontend/build/index.html`);
 });
 
 // catch 404 and forward to error handler
