651daa1e9dcf848f0b61679e8e77c10c74d9c562
Hristiana <2021813@swansea.ac.uk>
2022-08-28 16:28:36 +0100

    Adapted routes

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(function (req, res, next) {
 //train the AI
 trainAI.trainChatBotIA();
 
-// error handler
+//error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
